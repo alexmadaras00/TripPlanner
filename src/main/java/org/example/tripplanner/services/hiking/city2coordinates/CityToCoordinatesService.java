a2646ed43cd2f367d9282f09676b36505c57a341
MartinStoev00 <martinstoev00@gmail.com>
2024-03-29 17:47:23 +0100

    adding services

---
+++ src/main/java/org/example/tripplanner/services/hiking/city2coordinates/CityToCoordinatesService.java
@@ -0,0 +1,9 @@
+package org.example.tripplanner.services.hiking.city2coordinates;
+
+import java.util.ArrayList;
+
+
+public interface CityToCoordinatesService {
+    ArrayList getConvertedCityIntoCoordinatesWithSpringRest(String city);
+
+}

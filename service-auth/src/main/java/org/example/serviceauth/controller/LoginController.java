23688d57b694dabf5b0c6d3a23c04623384896a5
MartinStoev00 <martinstoev00@gmail.com>
2024-04-20 13:53:27 +0200

    login

--- service-auth/src/main/java/org/example/serviceauth/controller/LoginController.java
+++ service-auth/src/main/java/org/example/serviceauth/controller/LoginController.java
@@ -2,29 +2,45 @@ package org.example.serviceauth.controller;
 
 import org.example.serviceauth.data.User;
 import org.example.serviceauth.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
 
-    public LoginController(LoginService userService) {
-    }
-
-    @GetMapping("/")
-    public String loginRoot(Model model) {
-        model.addAttribute("user", new User());
-        return "login";
+    @PostMapping("/signup")
+    public ResponseEntity<String> singUp(@RequestBody User user) {
+        if (!loginService.saveUser(user)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists");
+        }
+        return ResponseEntity.ok("Signed up successfully!");
     }
 
     @PostMapping("/login")
-    public String loginPost(String username, String password, Model model) {
-
-        return "";
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            String jwt = loginService.checkUser(user);
+            return ResponseEntity.ok(jwt);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
-    // other controller methods
+    @PostMapping("/auth")
+    public ResponseEntity<String> auth(@RequestBody Map<String, String> requestBody) {
+        String id = requestBody.get("id");
+        String jwt = requestBody.get("jwt");
+        boolean ans = LoginService.verifyJwtId(id, jwt);
+        if (ans) {
+            return ResponseEntity.ok("Correct");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect");
+    }
 }
--- service-auth/src/main/java/org/example/serviceauth/repository/UserRepository.java
+++ service-auth/src/main/java/org/example/serviceauth/repository/UserRepository.java
@@ -3,7 +3,8 @@ package org.example.serviceauth.repository;
 
 import org.example.serviceauth.data.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-    User findByUsername(String username);
+    Mono<User> findByUsername(String username);
 }

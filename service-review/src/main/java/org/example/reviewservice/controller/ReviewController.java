763ccc0df6d59d51024b9a48691e7967526a9306
MartinStoev00 <martinstoev00@gmail.com>
2024-04-20 19:06:08 +0200

    review pages

---
+++ service-review/src/main/java/org/example/reviewservice/controller/ReviewController.java
@@ -0,0 +1,50 @@
+package org.example.reviewservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Objects;
+
+@Controller
+public class ReviewController {
+
+    @GetMapping("/reviewBefore")
+    public String getReviewBeforePage() {
+        return "reviewBefore";
+    }
+
+    @PostMapping(value = "/reviewBefore")
+    public ResponseEntity<String> postReviewBeforePage(@RequestBody MultiValueMap<String, String> formData) {
+        int destinationRating = Integer.parseInt(Objects.requireNonNull(formData.getFirst("destinations")));
+        int routeRating = Integer.parseInt(Objects.requireNonNull(formData.getFirst("routes")));
+        int scheduleRating = Integer.parseInt(Objects.requireNonNull(formData.getFirst("schedule")));
+        int bookingRating = Integer.parseInt(Objects.requireNonNull(formData.getFirst("booking")));
+
+        System.out.println(destinationRating + " " + routeRating + " " + scheduleRating + " " + bookingRating);
+
+        return ResponseEntity.ok("Review submitted successfully");
+    }
+
+    @GetMapping("/reviewAfter")
+    public String getReviewAfterPage() {
+        return "reviewAfter";
+    }
+
+    @PostMapping("/reviewAfter")
+    public ResponseEntity<String> postReviewAfterPage(@RequestBody MultiValueMap<String, String> formData) {
+        int arrived = Integer.parseInt(Objects.requireNonNull(formData.getFirst("arrived")));
+        int scheduleFollowed = Integer.parseInt(Objects.requireNonNull(formData.getFirst("schedule-followed")));
+        int accommodation = Integer.parseInt(Objects.requireNonNull(formData.getFirst("accommodation")));
+        int offers = Integer.parseInt(Objects.requireNonNull(formData.getFirst("offers")));
+        int overallExperience = Integer.parseInt(Objects.requireNonNull(formData.getFirst("overall-experience")));
+
+        System.out.println(arrived + " " + scheduleFollowed + " " + accommodation + " " + offers + " " + overallExperience);
+
+        return ResponseEntity.ok("Review submitted successfully");
+    }
+
+}

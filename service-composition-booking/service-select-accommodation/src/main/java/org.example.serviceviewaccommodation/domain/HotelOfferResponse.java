963afd0e21664adebf83fe8e6bf53ad915a8d951
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2024-04-22 14:15:38 +0200

    bugfix-booking-service-composition - Fixed ServiceSelectAccommodation - Fixed ServiceRecommendAccommodation - Fixed the controllers and htmls - Fixed the project structure and modules

--- service-composition-booking/service-select-accommodation/src/main/java/org.example.serviceviewaccommodation/domain/HotelOfferResponse.java
+++ service-composition-booking/service-select-accommodation/src/main/java/org.example.serviceviewaccommodation/domain/HotelOfferResponse.java
@@ -1,4 +1,4 @@
-package org.example.servicerecommendaccommodation.domain;
+package org.example.serviceviewaccommodation.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
